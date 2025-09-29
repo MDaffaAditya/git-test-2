c3e458c4756a3a6ee34610feb0143d1411177b2e
Daffa <mdaffaaditya2007@gmail.com>
2025-09-29 21:01:06 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque in atque exercitationem. Nihil, accusamus distinctio consequatur sapiente cupiditate esse qui.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
